67037545c61c14e3a37c86260eacfffa87f33eb0
Daniel Vitor Pereira da Silva <danielvitor081@gmail.com>
2021-07-02 15:24:28 -0300

    6

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,32 @@
+function Click(){
+    alert("Obrigado por dar esse Click ")
+}
+
+
+/*
+function soma(n1,n2) {
+    return n1 +n2;
+}
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome,novo_nome);
+}
+
+
+function validaIdade(idade) {
+    var validar;
+    if (idade>=18) {
+        validar=true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+var idade =prompt("Qual a sua idade ? ");
+console.log(validaIdade(idade));
+
+alert(soma(5,18));
+alert(setReplace("vai Japão", "Japão", "Brasil"));
+*/
 /*
 
 var d = new Date();
