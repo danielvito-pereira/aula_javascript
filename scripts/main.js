40ec33463c539126fad9bd04164e9509561126fb
Daniel Vitor Pereira da Silva <danielvitor081@gmail.com>
2021-07-02 14:02:16 -0300

    aula_1

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var nome = "Daniel Vitor";
+var n1= 23;
+var n2= 10;
+var frase = "Japão é o mehlor time do mundo ";
+//alert("Bem vindo "+nome+" Voce tem "+idade+" anos")
+console.log(nome);
+console.log(n1+n2);
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toLowerCase())
