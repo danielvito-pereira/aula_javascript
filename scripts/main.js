28a2d088c347ae7e64b69a836cf9ab16353cc668
Daniel Vitor Pereira da Silva <danielvitor081@gmail.com>
2021-07-02 14:27:01 -0300

    aula 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,46 @@
-var nome = "Daniel Vitor";
-var n1= 23;
-var n2= 10;
-var frase = "Japão é o mehlor time do mundo ";
+
+
+
+
+
+
+
+
+
+//var frutas = [{nome: "maça", cor:"vermelha"}, {nome:"uva", car:"roxa"}];
+//console.log(frutas.nome);
+//alert(ftutas.cor);
+
+
+
+
+
+
+//dicionario 
+//var fruta ={ nome:"maça", cor:"vermelho"};
+//console.log(fruta.nome);
+
+
+
+
+
+
+
+
+//var lista = ["maça","pera","laranja"];
+//lista.push("uva");//adiciona 
+//lista.pop("maça");//remove
+
+//console.log(lista[1]);
+//console.log(lista.length());//mostra o tamanho
+//console.log(lista.toString());
+//console.log(lista.join(" - "));//coloca qualquer coisa no meio de cada elemento para dividilos 
+//var nome = "Daniel Vitor";
+//var n1= 23;
+//var n2= 10;
+//var frase = "Japão é o mehlor time do mundo ";
 //alert("Bem vindo "+nome+" Voce tem "+idade+" anos")
-console.log(nome);
-console.log(n1+n2);
-console.log(frase.replace("Japão","Brasil"));
-console.log(frase.toLowerCase())
+//console.log(nome);
+//console.log(n1+n2);
+//console.log(frase.replace("Japão","Brasil"));
+//console.log(frase.toLowerCase());
